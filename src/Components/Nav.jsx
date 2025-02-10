5e72414c74062bfffe9ad09ec442c585f16c66e8
Fred Robins <fredrobins619@outlook.com>
2025-02-10 14:04:23 +0000

    Nav for all screens

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,9 @@
+function Nav () {
+    return (
+        <nav className="font-mono text-center p-4 bg-black text-white border-b-2 border-b-slate-400 sticky w-full">
+           <p> Google Flights Demo for <span className="text-blue-500 italic">Spotter</span></p>
+        </nav>
+    )
+}
+
+export default Nav
